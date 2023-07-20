66d98f695345dbe30841fd313d1bbfa49d37209f
xslmur <xslmr.proton.me>
2023-07-20 23:16:49 +0300

    set --headless option for webdriver

--- driver.py
+++ driver.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
 
 
 class Driver:
@@ -8,7 +9,9 @@ class Driver:
     @staticmethod
     def get_chrome_driver() -> WebDriver:
         if Driver.driver is None:
-            Driver.driver = webdriver.Chrome()
+            chrome_options = Options()
+            chrome_options.add_argument("--headless")
+            Driver.driver = webdriver.Chrome(options=chrome_options)
             Driver.driver.implicitly_wait(5)
             return Driver.driver
         else:
