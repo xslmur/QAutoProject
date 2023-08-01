df95afb9f4b35a61bcf1f5489265015c893a6114
xslmur <xslmr.proton.me>
2023-08-02 01:30:47 +0300

    capitalize the description of allure.step

--- facades/cars_facade.py
+++ facades/cars_facade.py
@@ -6,20 +6,18 @@ class CarsFacade(Cars):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    @allure.step("retreive cars via API")
+    @allure.step("Retreive cars via API")
     def get(self) -> list[Car]:
         return super().get()
 
-    @allure.step("create car via API")
+    @allure.step("Create car via API")
     def create(self, data: CarData) -> Car:
         return super().create(data)
 
-    @allure.step("delete cars via API")
+    @allure.step("Delete cars via API")
     def delete_cars(self, cars: list[Car]):
         return super().delete_cars(cars)
 
-    @allure.step("delete ALL cars via API")
+    @allure.step("Delete ALL cars via API")
     def delete_all_cars(self):
         return super().delete_cars()
-
-
